d8916161aeef839a459425c3d09a44ed10d29d6a
Hoshi-Kotaro <81416380+Hoshi-Kotaro@users.noreply.github.com>
2022-07-04 21:18:40 +0900

    Update sketch_geiger_counter.ino

--- testcode/sketch_geigerｰcounter/sketch_geiger_counter/sketch_geiger_counter.ino
+++ testcode/sketch_geigerｰcounter/sketch_geiger_counter/sketch_geiger_counter.ino
@@ -1,13 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial1.begin(38400, SERIAL_8N1,5, 23);
+  Serial.begin(115200, SERIAL_8N1,34,35);
+  Serial1.begin(38400, SERIAL_8N1,5,18);//RX,TX
+  Serial2.begin(115200, SERIAL_8N1,16,17);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(Serial1.available()){
-    char data = Serial1.read();
-    Serial.println(data);
+  while(Serial.available()){
+    char data = Serial.read();
+    Serial.print(data);
   }
 }
